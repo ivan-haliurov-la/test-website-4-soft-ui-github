0794bd90c8704a43fd0b4cee03554fb9820d2700
Ivan Haliurov <ivan.haliurov@levelaccess.com>
2025-12-22 13:50:25 +0200

    try dev env

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "@level-ci/cli";
 export default {
-  organization: "ivan-haliurov-7-userway-org",
-  project: "test-website-4-soft-ui-github-1",
+  organization: "ivan-haliurov-levelaccess-com-hspeq",
+  project: "test-website-4-soft-ui-github",
   token: process.env.LEVEL_CI_TOKEN,
   reportPaths: ["./level-ci-reports"],
-  server: "https://api.qa.userway.dev",
+  server: "https://api.dev.userway.dev",
 } satisfies Config;
